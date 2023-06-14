148e2690962141710df3f3c6bbaea9e2f36a0d18
arneto19 <armandopdneto@gmail.com>
2023-06-14 13:01:32 -0300

    feat: add linkedin link
    refactor: add target _blank on anchor tags; edit main techs

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -14,24 +14,29 @@ export const Heading = () => {
             <i className="fa-regular fa-calendar"></i>16/11/1994
           </p>
           <p>
-            <a href="https://api.whatsapp.com/send?phone=5579991214322">
+            <a href="https://api.whatsapp.com/send?phone=5579991214322" target="_blank">
               <i className="fa-brands fa-whatsapp"></i>(79) 99121-4322
             </a>
           </p>
           <p>
-            <a href="mailto:armandopdneto@gmail.com">
+            <a href="mailto:armandopdneto@gmail.com" target="_blank">
               <i className="fa-regular fa-envelope"></i>armandopdneto@gmail.com
             </a>
           </p>
           <p>
-            <a href="https://github.com/ArNeto19">
+            <a href="https://www.linkedin.com/in/armando-neto/" target="_blank">
+              <i className="fa-brands fa-linkedin"></i>LinkedIn
+            </a>
+          </p>
+          <p>
+            <a href="https://github.com/ArNeto19" target="_blank">
               <i className="fa-brands fa-github"></i>@ArNeto19
             </a>
           </p>
         </div>
         <div>
           <p>
-            <em>Javascript |TypeScript | React | React Native | Node.js</em>
+            <em>Javascript | TypeScript | React | React Native | Node.js</em>
           </p>
         </div>
       </div>
